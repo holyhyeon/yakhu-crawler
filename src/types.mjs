f54d3e99f35bf4ec0ce80b76d86891e10028c800
holyhyeon <ss9593014@gmail.com>
2026-09-10 16:36:15 +0900

    Create types.mjs

---
+++ src/types.mjs
@@ -0,0 +1,13 @@
+/**
+ * @typedef {Object} CollectedCandidate
+ * @property {'inven'} source
+ * @property {string} sourcePostId
+ * @property {string} sourceUrl
+ * @property {string} title
+ * @property {string} bodyText
+ * @property {string|null} publishedAt
+ * @property {string[]} mediaUrls
+ * @property {string} category
+ */
+
+export {};
